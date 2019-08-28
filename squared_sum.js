47f372efbc0c9d437d769558221b40e70647ea77
Ashwinduth <36796366+Ashwinduth@users.noreply.github.com>
2019-08-28 23:54:50 +0530

    Create squared_sum.js

---
+++ squared_sum.js
@@ -0,0 +1,10 @@
+<script>
+  var sum=0;
+  var i;
+  for(i=1;i<=5;i++){
+    var squared_Value=(i*i);
+    console.log("squared value of "+i+" is "+squared_Value);
+    sum=sum+squared_Value;
+  }
+  console.log("total sum is: "+sum);
+</script>
